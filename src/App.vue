97067928d353722187cf7ffcaec04ae1a6273b7c
Oleg Saveniuk <oleg.saveniuk@gmail.com>
2021-09-09 20:32:48 +0300

    Add helmet

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+console.log(window.magellanConfig, '----------------------------------')
+
 export default {
   name: 'App',
   components: {
